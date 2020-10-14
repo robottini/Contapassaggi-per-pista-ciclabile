0ccaddad24ec9733774bc6f6904da3e6b263a026
robottini <alegiaco@gmail.com>
2020-10-14 14:53:10 +0200

    Add files via upload

---
+++ Contapassaggi_Tcall_Final_da_parametrizzare/Contapassaggi_Tcall_Final_da_parametrizzare.ino
@@ -0,0 +1,349 @@
+#include "Arduino.h"
+#include "RTClib.h"
+
+
+// Your GPRS credentials (leave empty, if missing)
+const char apn[]      = "TM"; // Your APN                 //CHANGE THIS <-------
+const char gprsUser[] = ""; // User                       //CHANGE THIS <-------
+const char gprsPass[] = ""; // Password                   //CHANGE THIS <-------
+const char thingspeak_WriteAPIKey[] = "XXXXXXXXXXXXXXXX"; //CHANGE THIS <-------
+const char thingspeak_Channel[] = "YYYYYYY";              //CHANGE THIS <-------
+int anno = 20;                                             
+int mese = 7;                                              
+int giorno = 10;                                           
+int ora = 23;                                              
+int minuti = 54;                                           
+int secondi = 20;                                          
+int fuso = 1;
+
+int annoFirstData = 0;
+int meseFirstData = 0;
+int giornoFirstData = 0;
+
+char jsonStr[53] = "";
+char riga[150] = "";
+int passaggi[24] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int cumulati[24] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int batt[24] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int temp[24] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+String gsmDate = "";
+String gsmTime = "";
+
+int totaleGiorno = 0;
+
+int data_length = 0;
+byte ore = 24;
+boolean firstData = true;
+
+// Configure TinyGSM library
+#define SerialAT  Serial1
+#define TINY_GSM_MODEM_SIM800      // Modem is SIM800
+#define TINY_GSM_RX_BUFFER   64  // Set RX buffer to 1Kb
+
+// Define the serial console for debug prints, if needed
+//#define TINY_GSM_DEBUG Serial
+//#define DUMP_AT_COMMANDS
+
+#include <TinyGsmClient.h>
+TinyGsm modem(SerialAT);
+
+// Server details
+const char server[] = "api.thingspeak.com";
+
+TinyGsmClient client(modem);
+const int  port = 80;
+
+// TTGO T-Call pin definitions
+#define MODEM_RST            5
+#define MODEM_PWKEY          4
+#define MODEM_POWER_ON       23
+#define MODEM_TX             27
+#define MODEM_RX             26
+#define I2C_SDA              21
+#define I2C_SCL              22
+#define IP5306_ADDR          0x75
+#define IP5306_REG_SYS_CTL0  0x00
+
+#define BUZZER               25
+#define BUTTON_PIN_BITMASK 0x200000000 // 2^33 in hex
+#define uS_TO_S_FACTOR 1000000ULL
+#define SOUND_PWM_CHANNEL   0
+#define SOUND_RESOLUTION    8 // 8 bit resolution
+#define SOUND_ON            (1<<(SOUND_RESOLUTION-1)) // 50% duty cycle
+#define SOUND_OFF           0                         // 0% duty cycle
+esp_sleep_wakeup_cause_t wakeup_reason;
+
+RTC_Millis rtc;
+DateTime now;
+DateTime dt0;
+TimeSpan delta;
+
+const byte numChars = 32;
+char receivedChars[numChars] = "";
+boolean codeFound = false;
+
+boolean sent = false;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void setup() {
+
+
+
+  // Set-up modem reset, enable, power pins
+  pinMode(MODEM_PWKEY, OUTPUT);
+  pinMode(MODEM_RST, OUTPUT);
+  pinMode(MODEM_POWER_ON, OUTPUT);
+
+  pinMode(BUZZER, OUTPUT);
+
+  // Set console baud rate
+  Serial.begin(57600);
+  delay(4000);
+  Serial.println(F("Serial OK"));
+  gsmDate.reserve(10);
+  gsmTime.reserve(12);
+
+  // Keep power when running from battery
+  // Wire.begin(I2C_SDA, I2C_SCL);
+  // bool   isOk = setPowerBoostKeepOn(1);
+  // Serial.println(String("IP5306 KeepOn ") + (isOk ? "OK" : "FAIL"));
+
+  rtc.adjust(DateTime(anno, mese, giorno, ora, minuti, secondi));
+
+  delay(100);
+  now = rtc.now();
+  showDate("Start", now);
+
+  // Set GSM module baud rate and UART pins
+  SerialAT.begin(9600, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  delay(3000);
+  lampeggioLed(1);
+  Serial.println("Fine setup");
+  delay(1000);
+  dt0 = DateTime(anno, mese, giorno, ora, minuti + 1, 0);
+
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void loop() {
+
+
+  now = rtc.now();
+  delta = dt0 - now;
+  showTimeSpan("delta:", delta);
+
+  Serial.println("Going to sleep now");
+  Serial.flush();
+
+  esp_sleep_enable_ext1_wakeup(BUTTON_PIN_BITMASK, ESP_EXT1_WAKEUP_ANY_HIGH);
+  esp_sleep_enable_timer_wakeup(delta.totalseconds() * uS_TO_S_FACTOR);
+  esp_light_sleep_start();
+
+  delay(100);
+  print_wakeup_reason();
+  while (Serial.available()) {
+    Serial.read();
+  };
+  now = rtc.now();
+  showDate("RTC DateTime: ", now);
+  int hh = now.hour();
+  delay(100);
+
+  if (wakeup_reason == ESP_SLEEP_WAKEUP_EXT1) { /// interrupt from PIR
+    passaggi[hh] += 1;
+    cumulati[hh] += 1;
+    Serial.print(F("Passaggi:")); Serial.println(String(passaggi[hh]));
+    Serial.print(F("Cumulati:")); Serial.println(String(cumulati[hh]));
+    if (hh != 23)
+      cumulati[hh + 1] = cumulati[hh];
+    tone(BUZZER, 4000, 200);
+    delay(1800); //2000 - 200 buzzer
+
+  }
+
+  if (wakeup_reason == ESP_SLEEP_WAKEUP_TIMER ) {  /// interrupt from internal RTC
+    Serial.print(F("Passaggi:")); Serial.println(String(passaggi[hh]));
+    Serial.print(F("Cumulati:")); Serial.println(String(cumulati[hh]));
+
+    if (firstData) //lampeggia 2 volte, interrupt da RTC
+      lampeggioLed(2);
+
+    batt[hh] = 0;
+
+    if (hh == 5 || hh == 8 || hh == 11 || hh == 14 || hh == 17 || hh == 22) {
+      //         if (hh !=23 ) {
+
+      passaggi[hh + 1] = 0;
+      cumulati[hh + 1] = cumulati[hh];
+      ore = hh + 1;
+      int r = 0;
+      codeFound = false;
+      if ( hh == 22) {
+        totaleGiorno = cumulati[hh];
+        Serial.print(F("TotaleGiorno:")); Serial.println(String(totaleGiorno));
+      }
+      while (!codeFound && r < 3) {
+        Serial.print(F("Send data - Tentative:"));
+        Serial.println(r + 1);
+        sendData();
+        if (!codeFound) {
+          delay(20000);
+          r++;
+        }
+      }
+      if (codeFound)
+        sent = true;
+      Serial.println();
+    }
+
+    if (hh == 23) {
+      totaleGiorno = cumulati[hh];
+      Serial.print(F("TotaleGiorno:")); Serial.println(String(totaleGiorno));
+      ore = hh + 1;
+      int r = 0;
+      codeFound = false;
+      while (!codeFound && r < 3) {
+        Serial.print(F("Send data - Tentative:"));
+        Serial.println(r + 1);
+        sendData();
+        if (!codeFound) {
+          delay(20000);
+          r++;
+        }
+      }
+      if (codeFound)
+        sent = true;
+      Serial.println();
+      //    giornoSim += 1;
+      for (byte i = 0; i < 24; i++) {
+        passaggi[i] = 0;
+        cumulati[i] = 0;
+        batt[i] = 0;
+        temp[i] = 0;
+      }
+    }
+
+
+    if (hh != 5 && hh != 8 && hh != 11 && hh != 14 && hh != 17 &&  hh != 23 && hh != 22) {
+      //         if (hh != 23) {
+
+      passaggi[hh + 1] = 0;
+      cumulati[hh + 1] = cumulati[hh];
+    }
+
+    now = rtc.now();
+    anno = now.year();
+    mese = now.month();
+    giorno = now.day();
+    ora = now.hour();
+    minuti = now.minute();
+    secondi = now.second();
+    if (!sent) {
+      rtc.adjust(DateTime(anno, mese, giorno, ora, minuti, secondi));
+      now = rtc.now();
+    }
+    showDate("Now after sending:", now);
+
+    if (hh == 23)
+    {
+      if (firstData) {
+        anno = annoFirstData;
+        mese = meseFirstData;
+        giorno = giornoFirstData;
+        rtc.adjust(DateTime(anno, mese, giorno, ora, minuti, secondi));
+        now = rtc.now();
+        hh = now.hour();
+      }
+      dt0 = DateTime(anno, mese, giorno, 23, 55 , 0);
+      dt0 = dt0 + TimeSpan(0, 1, 0, 0);
+      int p = 40;
+      dt0 = dt0 + TimeSpan(0, 0, 0, p);
+    }
+
+    if (hh != 23)
+    {
+      hh = now.hour();
+      if (minuti < 55)
+        dt0 = DateTime(anno, mese, giorno, hh, 55 , 0);
+      else
+        dt0 = DateTime(anno, mese, giorno, hh + 1, 55 , 0);
+      int p = 40;
+      dt0 = dt0 + TimeSpan(0, 0, 0, p);
+    }
+    showDate("Dt0:", dt0);
+    firstData = false;
+    sent = false;
+  }
+}
+
+
+
+
+
+void lampeggioLed(byte t) {
+  for (byte i = 0; i < t; i++) {
+    tone(BUZZER, 2000, 300);
+    delay(300);
+  }
+}
+
+void tone(int pin, int frequency, int duration)
+{
+  ledcSetup(SOUND_PWM_CHANNEL, frequency, SOUND_RESOLUTION);  // Set up PWM channel
+  ledcAttachPin(pin, SOUND_PWM_CHANNEL);                      // Attach channel to pin
+  ledcWrite(SOUND_PWM_CHANNEL, SOUND_ON);
+  delay(duration);
+  ledcWrite(SOUND_PWM_CHANNEL, SOUND_OFF);
+}
+
+
+void print_wakeup_reason() {
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch (wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("\nWakeup caused by external signal using RTC");; break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("\nWakeup caused by external signal using PIR"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("\n****************\nWakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason); break;
+  }
+}
+
+
+void showDate(const char* txt, const DateTime& dt) {
+  Serial.print(txt);
+  Serial.print(' ');
+  Serial.print(dt.year(), DEC);
+  Serial.print('/');
+  Serial.print(dt.month(), DEC);
+  Serial.print('/');
+  Serial.print(dt.day(), DEC);
+  Serial.print(' ');
+  Serial.print(dt.hour(), DEC);
+  Serial.print(':');
+  Serial.print(dt.minute(), DEC);
+  Serial.print(':');
+  Serial.print(dt.second(), DEC);
+
+
+  Serial.println();
+}
+
+void showTimeSpan(const char* txt, const TimeSpan& ts) {
+  Serial.print(txt);
+  Serial.print(" ");
+  Serial.print(ts.days(), DEC);
+  Serial.print(" days ");
+  Serial.print(ts.hours(), DEC);
+  Serial.print(" hours ");
+  Serial.print(ts.minutes(), DEC);
+  Serial.print(" minutes ");
+  Serial.print(ts.seconds(), DEC);
+  Serial.print(" seconds (");
+  Serial.print(ts.totalseconds(), DEC);
+  Serial.print(" total seconds)");
+  Serial.println();
+}
---
+++ Contapassaggi_Tcall_Final_da_parametrizzare/GPRS.ino
@@ -0,0 +1,388 @@
+void sendData() {
+  // Mosfet for the power supply ON
+  Serial.println(F("Start grps"));
+  powerUp();
+  Serial.println(F("End start grps"));
+
+  // Restart takes quite some time
+  // To skip it, call init() instead of restart()
+  Serial.println(F("Initializing modem..."));
+  modem.restart();
+  // Or, use modem.init() if you don't need the complete restart
+
+  String modemInfo = modem.getModemInfo();
+  Serial.print(F("Modem: "));
+  Serial.println(modemInfo);
+  delay(500);
+
+
+  byte Vbatt = int(modem.getBattVoltage() / 100);
+  Serial.print(F("Batt (V):")); Serial.println(float(Vbatt) / 10.0);
+  //  Serial.print(F("Battery V:")); Serial.println(float(batt)/10.0);
+  delay(100);
+
+  Serial.println(F("Waiting for network..."));
+  if (!modem.waitForNetwork(40000L)) {
+    Serial.println(F(" fail"));
+    StopGprs();
+    return;
+  }
+  else {
+    Serial.println(F(" OK"));
+  }
+
+
+  if (modem.isNetworkConnected()) {
+    Serial.println(F("Network connected"));
+  }
+
+  Serial.print(F("Connecting to APN: "));
+  Serial.print(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+    Serial.println(F("  APN fail"));
+    StopGprs();
+    return;
+  }
+  else {
+    Serial.println(F("   APN OK"));
+  }
+
+
+  gsmTime = modem.getGSMDateTime(DATE_TIME);
+  Serial.print("GSM Time:"); Serial.println(gsmTime);
+  gsmDate = modem.getGSMDateTime(DATE_DATE);
+  Serial.print("GSM Date:"); Serial.println(gsmDate);
+  parseDateTime();
+
+  IPAddress local = modem.localIP();
+  Serial.print(F("Local IP: "));
+  Serial.println(local);
+  char appo3[3];
+  char appo6[6];
+  jsonStr[0] = '\0';
+  strcat(jsonStr, "{  \"write_api_key\": \"");
+  strcat(jsonStr, thingspeak_WriteAPIKey);
+  strcat(jsonStr, "\", \"updates\": [");
+  data_length = 52; //52 is the length of jsonStr
+  for (byte i = 0; i < ore; i++) {
+    riga[0] = '\0'; //clear the riga char array
+    strcat(riga, "{ \"created_at\": \"");
+
+    //Anno
+    strcat(riga, "20");
+    itoa(anno, appo3, 10);
+    strcat(riga, appo3);
+    strcat(riga, "-");
+
+    //Mese
+    appo3[0] = '0' + ((mese / 10) % 10); // tens digit
+    appo3[1] = '0' + (mese % 10);      // ones digit
+    appo3[2] = 0;        // ones digit
+    strcat(riga, appo3); //mese
+    strcat(riga,  "-");
+
+    //Giorno
+    appo3[0] = '0' + ((giorno / 10) % 10); // tens digit
+    appo3[1] = '0' + (giorno % 10);      // ones digit
+    appo3[2] = 0;        // ones digit
+    strcat(riga, appo3); //giorno
+    strcat(riga,  " ");
+
+    //ora
+    if ((i + 1) == 23 && ore == 23)
+      itoa(23, appo3, 10);
+    else
+      itoa(i, appo3, 10);
+    strcat(riga, appo3); // i
+
+    //minuti e secondi
+    //    strcat(riga, ":55:01 +0100");
+    strcat(riga, ":55:01 +");
+    appo3[0] = '0';
+    appo3[1] = '0' + (fuso / 4);
+    appo3[2] = 0;        // ones digit
+    strcat(riga, appo3);
+    strcat(riga, "00");
+
+
+    //campo 1 - passaggi orari
+    //campo 2 - passaggi orari cumulati
+    strcat(riga, "\", \"field1\":");
+    itoa(passaggi[i], appo6, 10);
+    strcat(riga, appo6);  //passaggi[i]
+    strcat(riga, ", \"field2\":");
+    itoa(cumulati[i], appo6, 10);
+    strcat(riga, appo6); //cumulati[i]
+    strcat(riga, ", \"field4\":");
+
+    //campo 4 - tensione batteria
+    if (ora == i)
+      batt[i] = Vbatt;
+    dtostrf( (float(batt[i]) / 10.0), 3, 1, appo6);
+    strcat(riga, appo6);
+
+    //campo 5 - temperatura
+    strcat(riga, ", \"field5\":");
+    dtostrf( 0, 3, 1, appo3);
+    strcat(riga, appo3);
+
+
+    if (i < (ore - 1)) {
+      strcat(riga, "},");
+    }
+    else {
+      if (i == 23|| i == 22) {
+        //campo 3 - passaggi giornalieri
+        strcat(riga, ", \"field3\":");
+        itoa(totaleGiorno, appo6, 10);
+        strcat(riga, appo6);
+      }
+
+      strcat(riga, "}]}");
+    }
+    data_length += strlen(riga);
+  }
+
+  data_length = data_length + 1;
+  Serial.print(F("Content-Length: ")); Serial.println(String(data_length));
+
+
+  Serial.print(F("Connecting to "));
+  Serial.println(server);
+
+  // lengthString();
+  client.stop();
+  delay(1000);
+
+
+  if (client.connect(server, 80)) {
+    client.print("POST /channels/");
+    client.print(thingspeak_Channel);
+    client.println("/bulk_update.json HTTP/1.1"); // Replace YOUR-CHANNEL-ID with your ThingSpeak channel ID
+    delay(50);
+    client.println(F("Host: api.thigspeak.com"));
+    delay(50);
+    client.println(F("Connection: close"));
+    delay(50);
+    client.println(F("Content-Type: application/json"));
+    delay(50);
+    client.println("Content-Length: " + String(data_length));
+    client.println();
+    delay(100);
+
+    client.print(jsonStr);
+    Serial.print(jsonStr);
+    delay(100);
+
+    for (byte i = 0; i < ore; i++) {
+      riga[0] = '\0'; //clear the riga char array
+      strcat(riga, "{ \"created_at\": \"");
+
+      //Anno
+      strcat(riga, "20");
+      itoa(anno, appo3, 10);
+      strcat(riga, appo3);
+      strcat(riga, "-");
+
+      //Mese
+      appo3[0] = '0' + ((mese / 10) % 10); // tens digit
+      appo3[1] = '0' + (mese % 10);      // ones digit
+      appo3[2] = 0;        // ones digit
+      strcat(riga, appo3); //mese
+      strcat(riga,  "-");
+
+      //Giorno
+      appo3[0] = '0' + ((giorno / 10) % 10); // tens digit
+      appo3[1] = '0' + (giorno % 10);      // ones digit
+      appo3[2] = 0;        // ones digit
+      strcat(riga, appo3); //giorno
+      strcat(riga,  " ");
+
+      //ora
+      if ((i + 1) == 23 && ore == 23)
+        itoa(23, appo3, 10);
+      else
+        itoa(i, appo3, 10);
+      strcat(riga, appo3); // i
+
+      //minuti e secondi
+      //strcat(riga, ":55:01 +0100");
+      strcat(riga, ":55:01 +");
+      appo3[0] = '0';
+      appo3[1] = '0' + (fuso / 4);
+      appo3[2] = 0;        // ones digit
+      strcat(riga, appo3);
+      strcat(riga, "00");
+
+      //campo 1 - passaggi orari
+      //campo 2 - passaggi orari cumulati
+      strcat(riga, "\", \"field1\":");
+      itoa(passaggi[i], appo6, 10);
+      strcat(riga, appo6);  //passaggi[i]
+      strcat(riga, ", \"field2\":");
+      itoa(cumulati[i], appo6, 10);
+      strcat(riga, appo6); //cumulati[i]
+
+
+      //campo 4 - tensione batteria
+      strcat(riga, ", \"field4\":");
+      //     if (ora == i)
+      //       batt[i] = Vbatt;
+      dtostrf( (float(batt[i]) / 10.0), 3, 1, appo6);
+      strcat(riga, appo6);
+
+      //campo 5 - temperatura
+      strcat(riga, ", \"field5\":");
+      dtostrf( 0, 3, 1, appo3);
+      strcat(riga, appo3);
+
+
+      if (i < (ore - 1)) {
+        strcat(riga, "},");
+      }
+      else {
+        if (i == 23 || i == 22) {
+          //campo 3 - passaggi giornalieri
+          strcat(riga, ", \"field3\":");
+          itoa(totaleGiorno, appo6, 10);
+          strcat(riga, appo6);
+        }
+
+        strcat(riga, "}]}");
+      }
+      client.print(riga);
+      Serial.println(riga);
+      // riga = "";
+      delay(100);
+    }
+    client.println();
+
+  }
+  else {
+    StopGprs();
+    Serial.println(F("Failure: Failed to connect to ThingSpeak"));
+    return;
+  }
+  delay(3000); //Wait to receive the response
+
+  boolean trovat = false;
+  int ndx = 0;
+  int endNdx = 23;
+  unsigned long timeout = millis();
+  while (client.connected() && millis() - timeout < 5000L) {
+    while (client.available() && ndx < endNdx) {
+      receivedChars[ndx] = client.read();
+      ndx++;
+    }
+    if (!trovat) {
+      Serial.print(F("receivedChars:")); Serial.println(receivedChars);
+      int g = 0;
+      while (g < endNdx - 8 && !codeFound) {
+        if (receivedChars[g] == '/') {
+          char code[4];
+          code[0] = receivedChars[g + 5];
+          code[1] = receivedChars[g + 6];
+          code[2] = receivedChars[g + 7];
+          code[3] = '\0';
+          Serial.print(F("Code:")); Serial.println(code);
+          if (strcmp(code, "202") == 0) {
+            Serial.println(F("Saved correctly"));
+            codeFound = true;
+            lampeggioLed(3);
+          }
+        }
+        g++;
+      }
+      Serial.println(F("Altri caratteri:"));
+      trovat = true;
+    }
+    char c = client.read();
+    Serial.print(c);
+    timeout = millis();
+  }
+
+  Serial.println();
+
+  // Shutdown
+  client.stop();
+  Serial.println(F("Server disconnected"));
+
+  modem.gprsDisconnect();
+  Serial.println(F("GPRS disconnected"));
+  passaggi[0] = 0;
+  cumulati[0] = 0;
+
+  // Mosfet for the power supply OFF
+  StopGprs();
+}
+
+void  StopGprs() {
+  Serial.println(F("Start stop gprs"));
+  powerDown();
+  Serial.println(F("End stop gprs"));
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void parseDateTime() {
+  //   GSM Time:17:41:34+04
+  //   GSM Date:19/12/28
+  int x = 0;
+  int y = 0;
+  x = gsmTime.indexOf(":");
+  String valore;
+  valore = gsmTime.substring(0, x);
+  ora = valore.toInt();
+  y = gsmTime.lastIndexOf(":");
+  valore = gsmTime.substring(x + 1, y);
+  minuti = valore.toInt();
+  x = gsmTime.indexOf("+");
+  valore = gsmTime.substring(y + 1, x);
+  secondi = valore.toInt();
+  valore = gsmTime.substring(x + 1, gsmTime.length());
+  fuso = valore.toInt();
+  Serial.println("Ora:" + String(ora) + " Minuti:" + String(minuti) + " Secondi:" + secondi + " Fuso:" + fuso);
+  x = gsmDate.indexOf("/");
+  valore = gsmDate.substring(0, x);
+  if (!firstData)
+    anno = valore.toInt();
+  else
+    annoFirstData = valore.toInt();
+  y = gsmDate.lastIndexOf("/");
+  valore = gsmDate.substring(x + 1, y);
+  if (!firstData)
+    mese = valore.toInt();
+  else
+    meseFirstData = valore.toInt();
+  valore = "";
+  valore = gsmDate.substring(y + 1, gsmDate.length());
+  if (!firstData)
+    giorno = valore.toInt();
+  else
+    giornoFirstData = valore.toInt();
+  if (firstData) {
+    DateTime firstDay = DateTime(annoFirstData, meseFirstData, giornoFirstData, ora, minuti , secondi);
+    firstDay = firstDay + TimeSpan(-1, 0, 0, 0);
+    mese = firstDay.month();
+    giorno = firstDay.day();
+  }
+  Serial.println("giorno:" + String(giorno) + " Mese:" + String(mese) + " Anno:" + String(anno));
+  rtc.adjust(DateTime(anno, mese, giorno, ora, minuti, secondi));
+}
+
+
+
+void powerUp()
+{
+  digitalWrite(MODEM_PWKEY, LOW);
+  digitalWrite(MODEM_RST, HIGH);
+  digitalWrite(MODEM_POWER_ON, HIGH);
+  delay(3000);
+}
+
+
+void powerDown()
+{
+  digitalWrite(MODEM_POWER_ON, LOW);
+  delay(1000);
+}
